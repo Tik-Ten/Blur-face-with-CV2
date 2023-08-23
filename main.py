aea2660eb731a250732300dfbff3eb56a0996834
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-08-23 18:51:14 +0330

    Project finished

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ Face_detector = FaceDetector(minDetectionCon=0.75)
 while True:
     rec, image = cap.read()
 
-    image, bbox = Face_detector.findFaces(image, )
+    image, bboxs = Face_detector.findFaces(image)
+
+    if bboxs:
+        for i, bbox in enumerate(bboxs):
+            x, y, w, h = bbox["bbox"]
+            if x < 0: x = 0
+            if y < 0: y = 0
+            imageCrop = image[y:y + h, x:x + w]
+            imageBlur = cv.blur(imageCrop, (500, 500))
+            image[y:y + h, x:x + w] = imageBlur
 
     cv.imshow("Blured face", image)
     cv.waitKey(1)
