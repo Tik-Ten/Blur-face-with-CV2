d03d1b10cbb1e7593a64a64e0e7803b230b2370c
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-08-23 18:28:32 +0330

    add face detector

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import cv2 as cv
+from cvzone.FaceDetectionModule import FaceDetector
 
 cap = cv.VideoCapture(1)
 
+Face_detector = FaceDetector(minDetectionCon=0.75)
 while True:
     rec, image = cap.read()
 
-    cv.imshow(image)
+    image, bbox = Face_detector.findFaces(image, )
+
+    cv.imshow("Blured face", image)
     cv.waitKey(1)
